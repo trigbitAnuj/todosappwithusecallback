9a75acbf099f55ee4054a0415715f9e523d79e1a
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-05 16:51:16 +0530

    main component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useCallback, useState } from 'react';
+import Todos from './Todos';
+import Count from './Count';
+
+const initialTodos=[
+  {id:1,text:"new todo"},
+  {id:2,text:"new todo"},
+  {id:3,text:"new todo"}
+]
+let nextId=4
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(initialTodos);
+  console.log("parent Component")
+
+const increment=useCallback(()=>{
+  console.log("count button Clicked")
+  setCount(count+1)
+},[count])
+
+const addTodo=useCallback(()=>{
+  console.log("add todo button clicked")
+  setTodos((todos)=>[...todos,{id:nextId++,text:"nextTodo"}])
+},[todos])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Count count={count} increment={increment}/>
+      <Todos todos={todos} addTodo={addTodo}/>
+
     </div>
+    
   );
 }
 
