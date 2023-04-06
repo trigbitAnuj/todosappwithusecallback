87d7ed06f4be822e147d3948eff35a9c11710153
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-06 17:23:00 +0530

    packages install

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+function AddTodo({onAddTodo}) {
+    const [text ,setText]=useState("")
+  return (
+    <section className='m-4  w-full flex justify-center'>
+         <input type="text"  value={text} onChange={(e)=>setText(e.target.value)} placeholder='add todo' className='p-3 text-xl border-black border-2 rounded-md w-[40%] ' />
+         <button className=' cursor-pointer p-2 mx-2 border outline-none  bg-blue-600 rounded-md text-xl hover:bg-blue-700 ' onClick={()=>{onAddTodo(text)
+         setText("")}}>AddTodo</button>
+    
+    </section>
+  )
+}
+
+export default AddTodo 
+
