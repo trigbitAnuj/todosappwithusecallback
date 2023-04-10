871b67ca437eebc47176947f256bf6e0651f95d3
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-10 11:22:08 +0530

    actioncreater

--- src/components/reducer.js
+++ src/components/reducer.js
@@ -1,18 +1,22 @@
 export function reducer(todos,action){
     switch(action.type){
        case "add" :
-           const {id,text}=action
+           const {id,text}=action.payload;
+           console.log([...todos,{id:id,text:text,isDone:false}])
            return [...todos,{id:id,text:text,isDone:false}];
+          
   
   
       case "remove":
-        const {todoId}=action;
-        return (todos.filter((todo)=>todo.id!==todoId));
-  
+        const {todoId}=action.payload;
+          return (todos.filter((todo)=>todo.id!==todoId));
+      
         case "change":
-          const {changeId}=action;
+          const {changeId}=action.payload;
           return (todos.map((todo)=>todo.id===changeId?{...todo,isDone:!todo.isDone}:todo))
-  
+        
+           default :
+           return todos;
   
     }
    }
