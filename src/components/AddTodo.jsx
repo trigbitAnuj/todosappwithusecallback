95e7ab3bfbeae026712d521c55f82d7932608e48
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-12 16:06:02 +0530

    refactor AddTodo component name

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,25 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-function AddTodo({dispatch}) {
-     
-   
-    const [text ,setText]=useState("")
+const AddTodo = ({ dispatch }) => {
+  const [text, setText] = useState("");
+
+  const addTodoButtonClick = () => {
+    dispatch({
+      type: "add",
+      payload: {
+        text: text,
+        id: Date.now(),
+      },
+    });
+    setText("");
+  };
   return (
-    <section className='m-4  w-full flex justify-center'>
-         <input type="text"  value={text} onChange={(e)=>setText(e.target.value)} placeholder='add todo' className='p-3 text-xl border-black border-2 rounded-md w-[40%] ' />
-         <button className=' cursor-pointer p-2 mx-2 border outline-none  bg-blue-600 rounded-md text-xl hover:bg-blue-700 ' onClick={()=>{dispatch({
-           type:"add",
-           payload:{
-            text:text,
-            id:Date.now()
-           }
-         })
-
-         setText("")}}>AddTodo</button>
-    
+    <section className="m-4  w-full flex justify-center">
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="add todo"
+        className="p-3 text-xl border-black border-2 rounded-md w-[40%] "
+      />
+      <button
+        className=" cursor-pointer p-2 mx-2 border outline-none  bg-blue-600 rounded-md text-xl hover:bg-blue-700 "
+        onClick={addTodoButtonClick}
+      >
+        AddTodo
+      </button>
     </section>
-  )
-}
+  );
+};
 
-export default AddTodo 
-
+export default AddTodo;
