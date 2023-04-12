11bbe5d02e64f1c2ac0f3912b35480e28a5cac75
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-12 16:04:25 +0530

    refactor component name

--- src/components/Completed.jsx
+++ src/components/Completed.jsx
@@ -2,46 +2,35 @@ import { Draggable } from "react-beautiful-dnd";
 import { MdDone } from "react-icons/md";
 import { AiFillDelete } from "react-icons/ai";
 
+const Completed = ({ index, todo, deleteCompletedTodos }) => {
+  console.log("completed component rendered");
 
-Draggable
-export default function Completed ({index,todo, onChangeCompletedTodos,onDeleteCompletedTodos}){
-  
-    return (
-      <Draggable draggableId={todo.id.toString()} index={index}>
-        {(provided) => (
-          <div
-            className="flex rounded-md  justify-between  mt-3  p-[15px] bg-gray-400 hover:scale-[1.01]"
-            {...provided.dragHandleProps}
-            {...provided.draggableProps}
-            ref={provided.innerRef}
-          >
-            {todo.isDone ? (
-              <s className="text-2xl font-light">{todo.text}</s>
-            ) : (
-              <span className="text-2xl">{todo.text}</span>
-            )}
-  
-            <section className="flex ">
-              <button
-                className="cursor-pointer ml-3 text-xl"
-                onClick={() =>onDeleteCompletedTodos(todo.id)
-                  
-                }
-              >
-                <AiFillDelete />
-              </button>
-              <button
-                className="cursor-pointer ml-3 text-xl"
-                onClick={() =>onChangeCompletedTodos(todo.id)
-                 
-                }
-              >
-                <MdDone />
-              </button>
-            </section>
-          </div>
-        )}
-      </Draggable>
-    );
-  
-  }
+  return (
+    <Draggable draggableId={todo.id.toString()} index={index}>
+      {(provided) => (
+        <div
+          className="flex rounded-md  justify-between  mt-3  p-[15px] bg-green-400 hover:scale-[1.01]"
+          {...provided.dragHandleProps}
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+        >
+          {todo.isDone ? (
+            <s className="text-2xl font-light">{todo.text}</s>
+          ) : (
+            <span className="text-2xl">{todo.text}</span>
+          )}
+
+          <section className="flex ">
+            <button
+              className="cursor-pointer ml-3 text-xl"
+              onClick={() => deleteCompletedTodos(todo.id)}
+            >
+              <AiFillDelete />
+            </button>
+          </section>
+        </div>
+      )}
+    </Draggable>
+  );
+};
+export default Completed;
