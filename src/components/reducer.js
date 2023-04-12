316f94e76941471668a3f2d98c74a3766d820651
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-12 16:03:51 +0530

    Update reducer.js

--- src/components/reducer.js
+++ src/components/reducer.js
@@ -1,22 +1,21 @@
-export function reducer(todos,action){
-    switch(action.type){
-       case "add" :
-           const {id,text}=action.payload;
-           console.log([...todos,{id:id,text:text,isDone:false}])
-           return [...todos,{id:id,text:text,isDone:false}];
-          
-  
-  
-      case "remove":
-        const {todoId}=action.payload;
-          return (todos.filter((todo)=>todo.id!==todoId));
-      
-        case "change":
-          const {changeId}=action.payload;
-          return (todos.map((todo)=>todo.id===changeId?{...todo,isDone:!todo.isDone}:todo))
-        
-           default :
-           return todos;
-  
-    }
-   }
+export const reducer = (todos, action) => {
+  switch (action.type) {
+    case "add":
+      const { id, text } = action.payload;
+
+      return [...todos, { id: id, text: text, isDone: false }];
+
+    case "remove":
+      const { todoId } = action.payload;
+      return todos.filter((todo) => todo.id !== todoId);
+
+    case "change":
+      const { changeId } = action.payload;
+      return todos.map((todo) =>
+        todo.id === changeId ? { ...todo, isDone: !todo.isDone } : todo
+      );
+
+    default:
+      return todos;
+  }
+};
