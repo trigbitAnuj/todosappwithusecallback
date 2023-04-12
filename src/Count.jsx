708cccf0f63658909ecd289770b96165ec71d760
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-12 16:01:48 +0530

    Update Count.jsx

--- src/Count.jsx
+++ src/Count.jsx
@@ -1,15 +1,15 @@
-import React, { memo, useEffect } from 'react'
+import React from "react";
 
-function Count({count,increment}) {
-    useEffect(()=>{
-        console.log("count component render")
-    })
+function Count({ count, increment }) {
+  React.useEffect(() => {
+    console.log("count component render");
+  });
   return (
-    <div>Count:{count}
-   <button onClick={increment}>Add Count</button>
+    <div>
+      Count:{count}
+      <button onClick={increment}>Add Count</button>
     </div>
-
-  )
+  );
 }
 
-export default memo(Count)
+export default React.memo(Count);
